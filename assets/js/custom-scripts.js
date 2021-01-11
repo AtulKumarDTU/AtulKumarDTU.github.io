a69ce6e411ee7cc5d275cbb771240e69f63792c2
Atul Kumar <absolute0gravity@gmail.com>
2021-01-12 02:58:25 +0530

    Relaunch

--- assets/js/custom-scripts.js
+++ assets/js/custom-scripts.js
@@ -32,7 +32,40 @@
           });
         });
       /* Loader Code End */
+
+
+      // var height = $('.mh-service-item').height();
+      // if($(window).width()){
+      //   $('.mh-service-item').css('height', height);   
+      //   $('.mh-service-item').css('height', height);   
+      // }
   
+
+      $(window).on('load', function() {
+        $('#header-slider #animation-slide').owlCarousel({
+               autoHeight: true,
+               items: 1,
+               loop: true,
+               autoplay: true,
+               dots: false,
+               nav: false,
+               autoplayTimeout: 3000,
+               navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+               animateIn: "zoomIn",
+               animateOut: "fadeOutDown",
+               autoplayHoverPause: false,
+               touchDrag: true,
+               mouseDrag: true
+           });
+         $("#animation-slide").on("translate.owl.carousel", function () {
+             $(this).find(".owl-item .slide-text > *").removeClass("fadeInUp animated").css("opacity","0");
+             $(this).find(".owl-item .slide-img").removeClass("fadeInRight animated").css("opacity","0");
+         });          
+         $("#animation-slide").on("translated.owl.carousel", function () {
+             $(this).find(".owl-item.active .slide-text > *").addClass("fadeInUp animated").css("opacity","1");
+             $(this).find(".owl-item.active .slide-img").addClass("fadeInRight animated").css("opacity","1");
+         });
+     });
    
     /*
     |====================
@@ -307,20 +340,20 @@
     
     
     // Smooth Scroll
-        // $(function() {
-        //   $('a[href*=#]:not([href=#])').click(function() {
-        //     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-        //       var target = $(this.hash);
-        //       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-        //       if (target.length) {
-        //         $('html,body').animate({
-        //           scrollTop: target.offset().top
-        //         }, 600);
-        //         return false;
-        //       }
-        //     }
-        //   });
-        // });
+        $(function() {
+          $('a[href*=#]:not([href=#])').click(function() {
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+              var target = $(this.hash);
+              target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+              if (target.length) {
+                $('html,body').animate({
+                  scrollTop: target.offset().top
+                }, 600);
+                return false;
+              }
+            }
+          });
+        });
         
         
         
